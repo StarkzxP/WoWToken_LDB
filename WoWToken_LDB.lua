3551ef4dabafd66957aab5584c21779356e1f00b
Starkz <starkzcc@gmail.com>
2024-10-18 20:51:01 -0300

    Made some renaming to keep everything consistent

--- WoWToken_LDB.lua
+++ WoWToken_LDB.lua
@@ -1,30 +1,30 @@
 local LDB = LibStub:GetLibrary("LibDataBroker-1.1")
 
 local dataObj = LDB:NewDataObject("Token price", {
-  label = "Token price",
-  type = "data source",
-  icon = "Interface\\Icons\\WoW_Token01",
-  text = GetMoneyString(0)
+    label = "Token price",
+    type = "data source",
+    icon = "Interface\\Icons\\WoW_Token01",
+    text = GetMoneyString(0)
 })
 
 local function UpdateDisplay()
-  local currentprice = C_WowTokenPublic.GetCurrentMarketPrice()
-  if currentprice ~= nil then
-    dataObj.text = GetMoneyString(currentprice)
-  end
+    local currentPrice = C_WowTokenPublic.GetCurrentMarketPrice()
+    if currentPrice ~= nil then
+        dataObj.text = GetMoneyString(currentPrice)
+    end
 end
 
 local function UpdatePriceReschedule()
-  C_WowTokenPublic.UpdateMarketPrice()
-  C_Timer.After(5, UpdateDisplay)
-  C_Timer.After(55, UpdatePriceReschedule)
+    C_WowTokenPublic.UpdateMarketPrice()
+    C_Timer.After(5, UpdateDisplay)
+    C_Timer.After(55, UpdatePriceReschedule)
 end
 
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_LOGIN")
 frame:SetScript("OnEvent", function(self, event)
-  if event == "PLAYER_LOGIN" then
-    UpdatePriceReschedule()
-    self:UnregisterEvent("PLAYER_LOGIN")
-  end
+    if event == "PLAYER_LOGIN" then
+        UpdatePriceReschedule()
+        self:UnregisterEvent("PLAYER_LOGIN")
+    end
 end)
